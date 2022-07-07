20cee5faba487f7100e23e92c44ee812320b42e2
rema35 <remaks35@gmail.com>
2022-07-07 12:37:19 +0200

    Random gif

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const key = require("./secret/key.json")
-const id_csgo_role = require("./secret/id.json")
-
+const id = require("./secret/id.json")
 const Discord = require("discord.js")
 const client = new Discord.Client({
     intents: [
@@ -14,8 +13,10 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
+    const messages = [" https://tenor.com/view/cs-go-lets-goo-ignuxaz-gif-25390531", "https://tenor.com/view/diego-eric-csgo-csgo-players-counter-strike-gif-22766889", "https://tenor.com/view/csgo-lmao-lmfao-wtf-creepy-gif-4690411", "https://tenor.com/view/csgo-dance-cs-go-dance-gif-24119623"]
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
     if(message.content == "/search") {
-        message.reply(`<@${message.author.id}> cherche un joueur <@&994042052994138182> !`)
+        message.reply(`<@${message.author.id}> cherche un joueur <@&994042052994138182> ! ${randomMessage}`) 
     }
     if(message.content == "c'est bien tu es obéissant") {
         message.reply("c'est un honneur maître")
@@ -24,4 +25,6 @@ client.on("messageCreate", (message) => {
         message.reply("oui grand maître")
     }
 })
+
+
 client.login(key.token_discord);
