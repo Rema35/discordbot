7b37ece4eea2d54d763f5ce47a8a4a1919d838f4
rema35 <remaks35@gmail.com>
2022-07-07 12:52:44 +0200

    Secret id roles

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const roleid = require("./secret/roleid.json")
 const key = require("./secret/key.json")
-const id = require("./secret/id.json")
+
 const Discord = require("discord.js")
 const client = new Discord.Client({
     intents: [
@@ -16,7 +17,7 @@ client.on("messageCreate", (message) => {
     const messages = [" https://tenor.com/view/cs-go-lets-goo-ignuxaz-gif-25390531", "https://tenor.com/view/diego-eric-csgo-csgo-players-counter-strike-gif-22766889", "https://tenor.com/view/csgo-lmao-lmfao-wtf-creepy-gif-4690411", "https://tenor.com/view/csgo-dance-cs-go-dance-gif-24119623"]
     const randomMessage = messages[Math.floor(Math.random() * messages.length)];
     if(message.content == "/search") {
-        message.reply(`<@${message.author.id}> cherche un joueur <@&994042052994138182> ! ${randomMessage}`) 
+        message.reply(`<@${message.author.id}> cherche un joueur <@&${roleid.id_csgo_role}> ! ${randomMessage}`) 
     }
     if(message.content == "c'est bien tu es obéissant") {
         message.reply("c'est un honneur maître")
