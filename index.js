25acd86b4765e28309ab9095686987a6c6f84691
rema35 <remaks35@gmail.com>
2022-07-07 03:09:38 +0200

    Change totally the code

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const key = require('./secret/key.json');
+const key = require("./secret/key.json")
 
-var Discord = require("discord.js");
-var Client = new Discord.Client({intents:[]});
+const Discord = require("discord.js")
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
 
-Client.on("ready", () => {
-    console.log("le bot marche");
-});
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
 
-Client.login(key.token_discord)
+client.on("messageCreate", (message) => {
+    if(message.content == "/search") {
+        message.reply(`<@${client.user.id}> cette personne un joueur!`)
+    }
+    if(message.content == "c'est bien tu es obéissant") {
+        message.reply("c'est un honneur maître")
+    }
+    if(message.content == "appelle moi grand maître à partie de maintenant") {
+        message.reply("oui grand maître")
+    }
+})
+client.login(key.token_discord);
